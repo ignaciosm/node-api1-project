38cdcf33c06fe64bb2df0920a82c868da393e135
ignaciosm <ignaciosm@gmail.com>
2019-12-03 08:52:20 -0500

    get request works

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// implement your API here
+const express = require('express');
+
+const server = express();
+
+const db = require('./data/db');
+
+server.get('/', (req, res) => {
+  res.send({ api: 'works!'})
+});
+
+server.get('/api/users', (req, res) => {
+  db.find()
+  .then(users => {
+    res.status(200).json(users);
+  })
+  .catch(error => {
+    console.log('error on GET /api/users', error);
+    res
+      .status(500)
+      .json("error")
+  });
+});
+
+const port = 4000;
+server.listen(port, ()=> 
+  console.log(`\n *** API WORKS *** \n`)  
+);
